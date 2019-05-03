592c0d527ff185d654a80523611246096fda0e69
Sid Mani <sid@sidmani.com>
2019-05-02 21:47:20 -0700

    optimized jets + trails

--- src/jet.js
+++ src/jet.js
@@ -2,57 +2,58 @@ const t = require('three');
 
 class Jet {
   constructor(settings, scale = 1) {
-    this.rSq = settings.terminationRadius ** 2;
+    this.r = settings.terminationRadius;
 
-    this.ev = settings.emissionVelocity * scale;
-    this.es = settings.emissionVelocity;
+    this.ev = settings.emissionVelocity;
+    this.scale = scale;
     this.group = new t.Group();
 
-    this.numPoints = Math.ceil(settings.terminationRadius / this.es);
+    this.numPoints = 2 * Math.ceil(settings.terminationRadius / settings.emissionVelocity);
     this.geometry = new t.BufferGeometry();
     const positions = new Float32Array(this.numPoints * 3);
+    const colors = new Float32Array(this.numPoints * 3);
+    this.geometry.addAttribute('color', new t.BufferAttribute(colors, 3));
     this.geometry.addAttribute('position', new t.BufferAttribute(positions, 3));
-    this.particles = new t.Points(this.geometry);
+    const material = new t.PointsMaterial({ vertexColors: t.VertexColors });
+    this.particles = new t.Points(this.geometry, material);
 
-    this.latestRemoved = 0;
     this.nextPoint = 0;
-    this.velocities = new Float32Array(this.numPoints * 3);
+    this.framesToEdge = new Float32Array(this.numPoints);
+    this.framesSinceStart = new Float32Array(this.numPoints);
     this.group.add(this.particles);
   }
 }
 
 Jet.prototype.increment = function increment(direction) {
   const positions = this.particles.geometry.attributes.position.array;
-  const velocities = this.velocities;
-
-  // assumption: matter doesn't overtake previously emitted matter
-  const newLR = (this.latestRemoved + 1) % this.numPoints;
-  const nTR = newLR * 3;
-  const maxDisSq = (positions[nTR] ** 2) + (positions[nTR + 1] ** 2) + (positions[nTR + 2] ** 2);
-  if (maxDisSq > this.rSq) {
-    velocities[nTR] = 0;
-    velocities[nTR + 1] = 0;
-    velocities[nTR + 2] = 0;
-    positions[nTR] = 0;
-    positions[nTR + 1] = 0;
-    positions[nTR + 2] = 0;
-    this.latestRemoved = newLR;
-  }
+  const colors = this.particles.geometry.attributes.color.array;
 
   const np = 3 * this.nextPoint;
-  velocities[np] = direction.x * this.ev;
-  velocities[np + 1] = direction.y * this.ev;
-  velocities[np + 2] = direction.z * this.ev;
-  positions[np] = 0;
-  positions[np + 1] = 0;
-  positions[np + 2] = 0;
+  const adjEV = this.ev / (1 - this.scale * direction.z * this.ev);
+  positions[np] = direction.x * adjEV * this.scale;
+  positions[np + 1] = direction.y * adjEV * this.scale;
+  positions[np + 2] = direction.z * adjEV * this.scale;
+  colors[np] = 0x7f * direction.z + 0x7f;
+  this.framesSinceStart[this.nextPoint] = 1;
+  this.framesToEdge[this.nextPoint] = Math.ceil(this.r / adjEV);
   this.nextPoint = (this.nextPoint + 1) % this.numPoints;
-  this.particles.geometry.attributes.position.needsUpdate = true;
 
-  const n = this.numPoints * 3;
-  for (let i = 0; i < n; i += 1) {
-    positions[i] += velocities[i];
+  for (let i = 0; i < this.numPoints; i += 1) {
+    const n = 3 * i;
+    if (this.framesToEdge[i] === 0) {
+      colors[n] = 0.9 * colors[n];
+    } else {
+      const s = 1 + 1 / this.framesSinceStart[i];
+      positions[n] *= s;
+      positions[n + 1] *= s;
+      positions[n + 2] *= s;
+      this.framesToEdge[i] -= 1;
+      this.framesSinceStart[i] += 1;
+    }
   }
+
+  this.particles.geometry.attributes.position.needsUpdate = true;
+  this.particles.geometry.attributes.color.needsUpdate = true;
 };
 
 module.exports = Jet;
