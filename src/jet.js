8734d28e3af0967bd827b8bb0446142786a12277
Sid Mani <sid@sidmani.com>
2019-05-11 13:43:54 -0700

    heat termination shock

--- src/jet.js
+++ src/jet.js
@@ -1,14 +1,15 @@
 const t = require('three');
+const cm = require('./colorMap');
 
 class Jet {
   constructor(settings, scale = 1) {
     this.r = settings.terminationRadius;
     this.cf = settings.terminationCoolingFactor;
     this.ev = settings.emissionVelocity * scale;
-    this.group = new t.Group();
 
     // figure out when strike time switches inc -> dec etc
-    this.numPoints = 4 * Math.ceil(this.r / settings.emissionVelocity);
+    this.numPoints = 3 * Math.ceil(this.r / settings.emissionVelocity);
+
     this.geometry = new t.BufferGeometry();
     const positions = new Float32Array(this.numPoints * 3);
     const colors = new Float32Array(this.numPoints * 3);
@@ -19,67 +20,76 @@ class Jet {
 
     this.next = 0;
     this.available = [];
+    this.cIdx = new Uint8Array(this.numPoints);
     this.framesToEdge = new Float32Array(this.numPoints);
     this.framesSinceStart = new Float32Array(this.numPoints);
-    this.group.add(this.particles);
-  }
-}
+    this.group = this.particles;
 
-function assign3(arr, pos, val) {
-  arr[pos] = val;
-  arr[pos + 1] = val;
-  arr[pos + 2] = val;
+    this.maxBrightness = (1 - settings.emissionVelocity) ** 3.2;
+  }
 }
 
 Jet.prototype.increment = function increment(angle, direction) {
-  const positions = this.particles.geometry.attributes.position.array;
-  const colors = this.particles.geometry.attributes.color.array;
-
-  for (let i = 0; i < this.next; i++) {
-    const n = 3 * i;
-    if (this.framesToEdge[i] > 0) {
-      const s = 1 + 1 / this.framesSinceStart[i];
-      positions[n] *= s;
-      positions[n + 1] *= s;
-      positions[n + 2] *= s;
-      this.framesSinceStart[i] += 1;
-      this.framesToEdge[i] -= 1;
-    } else if (this.framesToEdge[i] === 0) {
-      colors[n] *= this.cf;
-      colors[n+1] *= this.cf;
-      colors[n+2] *= this.cf;
-      if (colors[n] < 25) {
-        assign3(positions, i * 3, 0);
-        this.available.push(i);
-        this.framesToEdge[i] = -1;
-      }
-    }
-  }
-
-  console.log(this.next);
-
-  const adjEV = this.ev / (1 - direction.z * this.ev);
-  let k;
-  if (this.available.length > 0) {
-    k = this.available.pop();
-  } else {
-    k = this.next;
-    this.next += 1;
-  }
-  const np = k * 3;
-
-  positions[np] = direction.x * adjEV;
-  positions[np + 1] = direction.y * adjEV;
-  positions[np + 2] = direction.z * adjEV;
-  colors[np] = 0xff;
-  colors[np + 1] = 0;
-  colors[np + 2] = 0;
-
-  this.framesSinceStart[k] = 1;
-  this.framesToEdge[k] = Math.ceil(Math.abs(this.r / adjEV));
+  this.updatePoints();
+  this.emit(direction);
 
   this.particles.geometry.attributes.position.needsUpdate = true;
   this.particles.geometry.attributes.color.needsUpdate = true;
 };
 
+Jet.prototype.emit = function emit(direction) {
+  const p = this.particles.geometry.attributes.position.array;
+  const c = this.particles.geometry.attributes.color.array;
+
+  const adjEV = this.ev / (1 - direction.z * this.ev);
+  const k = this.available.pop() || this.next++;
+  const np = k * 3;
+
+  p[np] = direction.x * adjEV;
+  p[np + 1] = direction.y * adjEV;
+  p[np + 2] = direction.z * adjEV;
+
+  // calculate relativistic beaming factor
+  const B = Math.floor(this.maxBrightness / ((1 - this.ev * direction.z) ** 3.2) * 0xff);
+  c[np] = cm.R[B];
+  c[np + 1] = cm.G[B];
+  c[np + 2] = cm.B[B];
+  this.cIdx[k] = B;
+
+  this.framesSinceStart[k] = 1;
+  this.framesToEdge[k] = Math.ceil(Math.abs(this.r / adjEV));
+};
+
+Jet.prototype.updatePoints = function updatePoints() {
+  const p = this.particles.geometry.attributes.position.array;
+  const c = this.particles.geometry.attributes.color.array;
+
+  for (let i = 0; i < this.next; i += 1) {
+    const n = 3 * i;
+    if (this.framesToEdge[i] > 0) {
+      const s = 1 + 1 / this.framesSinceStart[i];
+      p[n] *= s;
+      p[n + 1] *= s;
+      p[n + 2] *= s;
+      this.framesSinceStart[i] += 1;
+      this.framesToEdge[i] -= 1;
+    } else if (this.framesToEdge[i] === 0) {
+      this.cIdx[i] = 0xff;
+      this.framesToEdge[i] = -1;
+    } else if (this.framesToEdge[i] === -1) {
+      if (this.cIdx[i] < 20) {
+        p[n] = 0;
+        p[n + 1] = 0;
+        p[n + 2] = 0;
+        this.available.push(i);
+        this.framesToEdge[i] = -2;
+      } else {
+        const x = this.cIdx[i]--;
+        c[n] = cm.R[x];
+        c[n + 1] = cm.G[x];
+        c[n + 2] = cm.B[x];
+      }
+    }
+  }
+};
 module.exports = Jet;
