9f10e764a9c0a424e4248b825984b667b868aa20
Sid Mani <sid@sidmani.com>
2019-07-03 01:23:05 -0700

    rename for gh-pages

---
+++ docs/settings.js
@@ -0,0 +1,23 @@
+const Settings = {
+  default: {
+    pAxisTheta: 1.3,
+    pAxisPhi: -0.2,
+    openingAngle: 0.2708,
+    precRate: 5,
+    emissionVelocity: 0.2,
+    terminationRadius: 400,
+    terminationCoolingFactor: 0.99,
+  },
+};
+
+Settings.get = function getSettings() {
+  let saved;
+  if (saved = window.sessionStorage.getItem('settings')) {
+    return JSON.parse(saved);
+  }
+  return Settings.default;
+};
+
+Settings.set = function updateSettings(obj) {
+  window.sessionStorage.setItem('settings', JSON.stringify(obj));
+};
